9a3629d513f7bbb68284c6360909115a49c76a44
Arjun Gasikanti <AGasikanti@ad.prolifics.com>
2018-04-15 19:18:49 +0530

    SystemPerformance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular napp';
+  names=["Arjun","Vivek","Deepali"];
+  
+   display(txtBx1:string)
+  {
+    this.title=txtBx1+" hey you clicked me..";
+  
+  }
 }
